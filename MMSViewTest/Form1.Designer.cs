18c3c5def623e1a9955b5b1598e5989b99fc501c
SLAF <SLAF@AFHQ-ITU-SOFT1>
2023-04-27 09:54:38 +0530

    bcview

---
+++ MMSViewTest/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace MMSViewTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ptype = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ServiceNo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.pdid = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Initials = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Surname = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Test = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.rqdate = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Print = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle4;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ptype,
+            this.ServiceNo,
+            this.pdid,
+            this.Initials,
+            this.Surname,
+            this.Test,
+            this.rqdate,
+            this.Print});
+            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle6.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle6.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle6.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle6.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle6.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle6;
+            this.dataGridView1.Location = new System.Drawing.Point(11, 2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(1160, 356);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // ptype
+            // 
+            this.ptype.HeaderText = "";
+            this.ptype.Name = "ptype";
+            this.ptype.ReadOnly = true;
+            // 
+            // ServiceNo
+            // 
+            this.ServiceNo.HeaderText = "ServiceNo";
+            this.ServiceNo.Name = "ServiceNo";
+            this.ServiceNo.ReadOnly = true;
+            // 
+            // pdid
+            // 
+            this.pdid.HeaderText = "pdid";
+            this.pdid.Name = "pdid";
+            this.pdid.ReadOnly = true;
+            this.pdid.Visible = false;
+            // 
+            // Initials
+            // 
+            this.Initials.HeaderText = "Initials";
+            this.Initials.Name = "Initials";
+            this.Initials.ReadOnly = true;
+            // 
+            // Surname
+            // 
+            this.Surname.HeaderText = "Surname";
+            this.Surname.Name = "Surname";
+            this.Surname.ReadOnly = true;
+            this.Surname.Width = 260;
+            // 
+            // Test
+            // 
+            this.Test.HeaderText = "Test";
+            this.Test.Name = "Test";
+            this.Test.ReadOnly = true;
+            this.Test.Width = 260;
+            // 
+            // rqdate
+            // 
+            dataGridViewCellStyle5.Format = "D";
+            dataGridViewCellStyle5.NullValue = null;
+            this.rqdate.DefaultCellStyle = dataGridViewCellStyle5;
+            this.rqdate.HeaderText = "Date";
+            this.rqdate.Name = "rqdate";
+            this.rqdate.ReadOnly = true;
+            this.rqdate.Width = 200;
+            // 
+            // Print
+            // 
+            this.Print.HeaderText = "Print";
+            this.Print.Name = "Print";
+            this.Print.ReadOnly = true;
+            this.Print.Text = "Print";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 381);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            this.textBox1.Leave += new System.EventHandler(this.textBox1_Leave);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1183, 413);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ptype;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ServiceNo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn pdid;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Initials;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Surname;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Test;
+        private System.Windows.Forms.DataGridViewTextBoxColumn rqdate;
+        private System.Windows.Forms.DataGridViewButtonColumn Print;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ MMSViewTest/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MMSViewTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection oSqlConnection;
+        SqlCommand oSqlCommand;
+        SqlDataAdapter oSqlDataAdapter;
+        public string sqlQuery;
+        public string UserValue = "";
+        private void LoadPersonal2(string id)
+        {
+            DateTime batchdate = new DateTime();
+            DataSet odsLoadLoanAppliedPersonal = new DataSet();
+            odsLoadLoanAppliedPersonal.Clear();
+            dataGridView1.Rows.Clear();
+            DataSet odsvoltxndata = new DataSet();
+
+
+            String strDBCon = "Data Source = 135.22.210.105; Initial Catalog = MMS; User ID = mmsuser; Password = password;";
+            oSqlConnection = new SqlConnection(strDBCon);
+            oSqlCommand = new SqlCommand();
+            sqlQuery = "SELECT distinct f.Relationship,a.TestSID,e.CategoryName,c.serviceNo,c.Surname,c.Initials,CAST(a.[RequestedTime] as DATE) FROM [MMS].[dbo].[Lab_Report] as a left join [MMS].[dbo].[Patient_Detail] as b on a.PDID=b.PDID inner join [MMS].[dbo].[Patient] as  c  on c.PID=b.PID inner join  [MMS].[dbo].[Lab_SubCategory] as d on d.[LabTestID]=a.[LabTestID] inner join [MMS].[dbo].[Lab_MainCategory] as e on e.CategoryID=d.CategoryID inner join [MMS].[dbo].[RelationshipType] as f on c.RelationshipType=f.RTypeID  where   a.TestSID='" + id + "' ";
+
+            oSqlCommand.Connection = oSqlConnection;
+            oSqlCommand.CommandText = sqlQuery;
+
+            oSqlDataAdapter = new SqlDataAdapter(oSqlCommand);
+            oSqlConnection.Open();
+            oSqlDataAdapter.Fill(odsLoadLoanAppliedPersonal);
+            oSqlConnection.Close();
+            if (odsLoadLoanAppliedPersonal.Tables[0].Rows.Count > 0)
+            {
+                for (int count = 0; count < odsLoadLoanAppliedPersonal.Tables[0].Rows.Count; count++)
+                {
+                    dataGridView1.Rows.Add();
+                    dataGridView1.Rows[count].Cells["ServiceNo"].Value = odsLoadLoanAppliedPersonal.Tables[0].Rows[count]["ServiceNo"].ToString();
+                    dataGridView1.Rows[count].Cells["Initials"].Value = odsLoadLoanAppliedPersonal.Tables[0].Rows[count]["Initials"].ToString();
+                    dataGridView1.Rows[count].Cells["Surname"].Value = odsLoadLoanAppliedPersonal.Tables[0].Rows[count]["Surname"].ToString();
+                    dataGridView1.Rows[count].Cells["Test"].Value = odsLoadLoanAppliedPersonal.Tables[0].Rows[count]["CategoryName"].ToString();
+                    dataGridView1.Rows[count].Cells["pdid"].Value = odsLoadLoanAppliedPersonal.Tables[0].Rows[count]["TestSID"].ToString();
+                    dataGridView1.Rows[count].Cells["ptype"].Value = odsLoadLoanAppliedPersonal.Tables[0].Rows[count]["Relationship"].ToString();
+                    dataGridView1.Rows[count].Cells["rqdate"].Value = odsLoadLoanAppliedPersonal.Tables[0].Rows[count]["Column1"];
+                }
+            }
+            textBox1.Text = "";
+
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.ActiveControl = textBox1;
+        }
+
+        private void textBox1_Leave(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                LoadPersonal2(this.textBox1.Text);
+                this.ActiveControl = textBox1;
+            }
+        }
+    }
+}
